87e6b1444db2b595ad0aa7d2c3650c40e1a96c1d
Jared <jhbarnum@gmail.com>
2018-07-25 17:13:54 -0400

    style

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} style={{color: blue, marginBottom: 10 }} className="btn btn-success">
+  <button {...props} style={{marginBottom: 1 }} className="btn btn-success">
     {props.children}
   </button>
 );
