8c9f31ecd4d51e192e040b6b5510d2a3db0cc44a
Jared <jhbarnum@gmail.com>
2018-07-26 21:39:45 -0400

    img

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,22 @@
 import React from "react";
+import phones from "./polyheadphone.jpg";
 
 const Jumbotron = ({ children }) => (
+  <div class="row">
   <div
-    style={{ backgroundImage: "carbonf.png", height: 200, clear: "both", paddingTop: 50, textAlign: "center" }}
+    style={{ backgroundImage: "coffeevinyl.jpg", height: 200, clear: "both", paddingTop: 50, textAlign: "center" }}
  
     className="jumbotron"
   >
     {children}
+    
+      <img src={phones} style={{ height: 400, clear: "both", paddingTop: 5, align: "left" }}></img>
+    </div>
   </div>
 );
 
 export default Jumbotron;
+
+
+
+//style = {{ backgroundImage: "carbonf.png", height: 200, clear: "both", paddingTop: 50, textAlign: "center" }}
